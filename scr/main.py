730eb3b00148e8999b3c9fa4198c9fa3f2f0133d
aria <yizongling@gmail.com>
2019-05-29 07:01:18 +0800

    增加一点自动回复预设 开启关闭功能

--- scr/main.py
+++ scr/main.py
@@ -1,14 +1,17 @@
 # -*- coding: utf-8 -*-
+import string
+
 from itchat.content import *
 import requests
 import json
 import itchat
 import random
 
-global beenAT
-global item
+global robotActive
+global randomNum
 # global userName ='人工智障'
 itchat.auto_login()
+robotActive=1
 # itchat.auto_login(hotReload = True)
 # itchat.send(u'itchat 上线了', toUserName=searchFriendsID[u'等~等灯等灯~'])
 itchat.send(u'itchat 上线了', toUserName=u'小冰')
@@ -17,10 +20,10 @@ itchat.send(u'等~等灯等灯~', toUserName=u'文件传输助手')
 
 def dealAnswer(answer):
     if(answer=='今日水群结束'):
-        random=random.randint(0,4)
-        if(random==0):
+        randomNum=random.randint(0,4)
+        if(randomNum==0):
             answer='余额不足，请及时充值'
-        elif(random==1):
+        elif(randomNum==1):
             answer = '今日水群余额已不足，明日再来'
 
 # 调用图灵机器人的api，采用爬虫的原理，根据聊天消息返回回复内容
@@ -43,6 +46,10 @@ def group_id(name):
 # text_reply msg_files可以处理好友之间的聊天回复
 @itchat.msg_register([TEXT, MAP, CARD, NOTE, SHARING])
 def text_reply(msg):
+    global robotActive
+    if(msg['Content']=='start entry sequence'):
+        robotActive=1
+        itchat.send(u'启动！！！', msg['FromUserName'])
     itchat.send(u'我现在不在线', msg['FromUserName'])
     print(msg['FromUserName'])
     # itchat.send('%s' % tuling(msg['Text']),msg['FromUserName'])
@@ -58,38 +65,51 @@ def download_files(msg):
 @itchat.msg_register(TEXT, isGroupChat=True)
 def group_text_reply(msg):
     # 当然如果只想针对@你的人才回复，可以设置if msg['isAt']:
-
+    global robotActive
     item = group_id(u'知乎深圳大家庭')  # 根据自己的需求设置
     print('groupID' + item)
     print('2UserName' + msg['ToUserName'])
     print('FromUserName' + msg['FromUserName'])
     # print 'StatusNotifyCode'+msg['StatusNotifyCode']
-
-    if msg['isAt']:
-        answer = ''
-        randomNum=0
-        print(u'wa!!!被猪@了')
-        print(msg['ToUserName'])
+    if(robotActive):
         if msg['FromUserName'] == item:
-            print(u'知乎深圳大家庭')
+            answer = ''
+            randomNum=0
+
             content = msg['Content']
             content = content.strip('@人工智障 ')
-            print('@人工智障 ' + content)
-            if(content==''):
-                randomNum=random.randint(0,4)
-                if(randomNum==0):
-                    answer='爪子'
-                elif(randomNum ==1 ):
-                    answer = '哇，我被猪@了'
-                elif (randomNum == 2):
-                    answer = '???'
-                elif (randomNum == 3):
-                    answer = '有事说撒'
-                elif (randomNum == 4):
-                    answer = '！！！'
+
+            print(u'wa!!!被猪@了')
+            print(msg['ToUserName'])
+            if msg['isAt']:
+                print(u'知乎深圳大家庭')
+
+                print('@人工智障 ' + content)
+                if(content==''):
+                    randomNum=random.randint(0,6)
+                    if(randomNum==0):
+                        answer='爪子'
+                    elif(randomNum ==1 ):
+                        answer = '哇，我被猪@了'
+                    elif (randomNum == 2):
+                        answer = '???'
+                    elif (randomNum == 3):
+                        answer = '有事说撒'
+                    elif (randomNum == 4):
+                        answer = '在！'
+                    elif (randomNum == 5):
+                        answer = '谁在叫我'
+                else:
+                    answer=tuling(content)
+                    print(answer)
+                    if(content=='shutdown -s -t'):
+                        robotActive=0
+                        answer = 'bye~bye~'
+
             else:
-                answer=tuling(content)
-                print(answer)
+                if(content.find('人工智障')):
+                    pass
+                    # answer='谁在叫我？'
             itchat.send(answer, item)
 
 
